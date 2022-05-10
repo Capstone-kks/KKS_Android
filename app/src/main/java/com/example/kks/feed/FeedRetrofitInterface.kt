f05c25261367b88dc563828635eed6611fc77c9c
kuk329 <kuk329@cau.ac.kr>
2022-05-10 23:34:21 +0900

    더미데이터 추가

--- app/src/main/java/com/example/kks/feed/FeedRetrofitInterface.kt
+++ app/src/main/java/com/example/kks/feed/FeedRetrofitInterface.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface FeedRetrofitInterface {
     // Feed 목록 불러오기
-    @GET("/feed?")
+    @GET("/all?")
     fun getFeed(
         @Query("sort") sort :Int, // 정렬 기준 (최신순 : 1, 인기순 : 2)
         @Query("isFollowing")  isFollowing : Boolean //  팔로잉한 유저만(true)/ 전체 (false)
