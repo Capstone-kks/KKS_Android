d0f6275822d73ce8c1b42bf80fb10f51c1e0dfb1
bacchusdd <jinny3685@naver.com>
2022-05-30 12:29:28 +0900

    [수정-완료] 서버 배포 후 base-url 수정 완료

--- app/src/main/java/com/example/kks/NetworkModule.kt
+++ app/src/main/java/com/example/kks/NetworkModule.kt
@@ -3,7 +3,8 @@ package com.example.kks
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL="http://10.0.2.2:8080"
+//const val BASE_URL="http://10.0.2.2:8080"
+const val BASE_URL="http://ec2-54-180-169-133.ap-northeast-2.compute.amazonaws.com:8080";
 
 fun getRetrofit(): Retrofit {
     val retrofit = Retrofit.Builder()
