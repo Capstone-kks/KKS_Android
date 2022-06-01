50971a99ca33e846f8c666c9bd0219dac46a62b0
kuk329 <kuk329@cau.ac.kr>
2022-06-01 14:19:23 +0900

    팔로우/팔로워 화면 디자인 수정

--- app/src/main/java/com/example/kks/info/follow/FollowActivity.java
+++ app/src/main/java/com/example/kks/info/follow/FollowActivity.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +27,8 @@ public class FollowActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private FollowAdapter adapter;
+    private TextView followTitle;
+    private ImageView backBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,6 +37,9 @@ public class FollowActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.follow_activity);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), RecyclerView.VERTICAL, false));
+        followTitle=findViewById(R.id.followTitleTv);
+        backBtn=findViewById(R.id.followBackIv);
+
 
         Intent getintent = getIntent();
         userId = getintent.getStringExtra("userId");
@@ -42,12 +50,21 @@ public class FollowActivity extends AppCompatActivity {
         LoginUserId = sharedPreferences.getString("userId","");
 
         if(follow_num == 0) {
+            followTitle.setText("내가 팔로우한 사용자 목록");
             adapter = new FollowAdapter(this, followingList, userId, LoginUserId);
             recyclerView.setAdapter(adapter);
         }else if(follow_num == 1) {
-
+            followTitle.setText("나를 팔로우한 사용자 목록");
             adapter = new FollowAdapter(this, followerList, userId, LoginUserId);
             recyclerView.setAdapter(adapter);
         }
+
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
